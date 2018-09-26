06391534c2f458e9c3800d48451f7c166fce664c
Mevericks <reloved@gmail.com>
2018-09-26 14:46:01 +0300

    вывод секции команда

--- resources/views/site/content.blade.php
+++ resources/views/site/content.blade.php
@@ -124,7 +124,7 @@
       @foreach($portfolios as $item)
       <!-- Portfolio Item -->
       <div style="position: absolute; left: 0px; top: 0px; transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1); width: 337px; opacity: 1;" class="portfolio-item one-four {{$item->filter}} isotope-item">
-        <div class="portfolio_img"> {!! Html::image('assets/img/'.$item->images,$item->name) !!} </div>
+        <div class="portfolio_img"> {{ Html::image('assets/img/'.$item->images,$item->name) }} </div>
         <div class="item_overlay">
           <div class="item_info"> 
             <h4 class="project_name">{{$item->name}}</h4>
@@ -152,76 +152,28 @@
 <!--/Portfolio -->
 @endif
 
-<section class="page_section" id="clients"><!--page_section-->
-  <h2>Clients</h2>
-<!--page_section-->
-<div class="client_logos"><!--client_logos-->
-  <div class="container">
-    <ul class="fadeInRight animated wow">
-      <li><a href="javascript:void(0)"><img src="img/client_logo1.png" alt=""></a></li>
-      <li><a href="javascript:void(0)"><img src="img/client_logo2.png" alt=""></a></li>
-      <li><a href="javascript:void(0)"><img src="img/client_logo3.png" alt=""></a></li>
-      <li><a href="javascript:void(0)"><img src="img/client_logo5.png" alt=""></a></li>
-    </ul>
-  </div>
-</div>
-</section>
-<!--client_logos-->
-
+@if( isset($peoples) && is_object($peoples) )
 <section class="page_section team" id="team"><!--main-section team-start-->
   <div class="container">
     <h2>Team</h2>
     <h6>Lorem ipsum dolor sit amet, consectetur adipiscing.</h6>
     <div class="team_section clearfix">
+    @foreach($peoples as $k => $item)
       <div class="team_area">
-        <div class="team_box wow fadeInDown delay-03s">
+        <div class="team_box wow fadeInDown delay-0{{($k*3)}}s">
           <div class="team_box_shadow"><a href="javascript:void(0)"></a></div>
-          <img src="{{asset('assets/img/team_pic1.jpg')}}" alt="">
-          <ul>
-            <li><a href="javascript:void(0)" class="fa fa-twitter"></a></li>
-            <li><a href="javascript:void(0)" class="fa fa-facebook"></a></li>
-            <li><a href="javascript:void(0)" class="fa fa-pinterest"></a></li>
-            <li><a href="javascript:void(0)" class="fa fa-google-plus"></a></li>
-          </ul>
+          {{ Html::image('assets/img/'.$item->image,$item->name) }}
         </div>
-        <h3 class="wow fadeInDown delay-03s">Tom Rensed</h3>
-        <span class="wow fadeInDown delay-03s">Chief Executive Officer</span>
-        <p class="wow fadeInDown delay-03s">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat sollicitudin cursus. Dolor sit amet, consectetur adipiscing elit proin consequat.</p>
+        <h3 class="wow fadeInDown delay-0{{($k*3)}}s">{{$item->name}}</h3>
+        <span class="wow fadeInDown delay-0{{($k*3)}}s">{{ $item->position }}</span>
+        <p class="wow fadeInDown delay-0{{($k*3)}}s">{{ $item->bio }}</p>
       </div>
-      <div class="team_area">
-        <div class="team_box  wow fadeInDown delay-06s">
-          <div class="team_box_shadow"><a href="javascript:void(0)"></a></div>
-          <img src="{{asset('assets/img/team_pic2.jpg')}}" alt="">
-          <ul>
-            <li><a href="javascript:void(0)" class="fa fa-twitter"></a></li>
-            <li><a href="javascript:void(0)" class="fa fa-facebook"></a></li>
-            <li><a href="javascript:void(0)" class="fa fa-pinterest"></a></li>
-            <li><a href="javascript:void(0)" class="fa fa-google-plus"></a></li>
-          </ul>
-        </div>
-        <h3 class="wow fadeInDown delay-06s">Kathren Mory</h3>
-        <span class="wow fadeInDown delay-06s">Vice President</span>
-        <p class="wow fadeInDown delay-06s">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat sollicitudin cursus. Dolor sit amet, consectetur adipiscing elit proin consequat.</p>
-      </div>
-      <div class="team_area">
-        <div class="team_box wow fadeInDown delay-09s">
-          <div class="team_box_shadow"><a href="javascript:void(0)"></a></div>
-          <img src="{{asset('assets/img/team_pic3.jpg')}}" alt="">
-          <ul>
-            <li><a href="javascript:void(0)" class="fa fa-twitter"></a></li>
-            <li><a href="javascript:void(0)" class="fa fa-facebook"></a></li>
-            <li><a href="javascript:void(0)" class="fa fa-pinterest"></a></li>
-            <li><a href="javascript:void(0)" class="fa fa-google-plus"></a></li>
-          </ul>
-        </div>
-        <h3 class="wow fadeInDown delay-09s">Lancer Jack</h3>
-        <span class="wow fadeInDown delay-09s">Senior Manager</span>
-        <p class="wow fadeInDown delay-09s">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat sollicitudin cursus. Dolor sit amet, consectetur adipiscing elit proin consequat.</p>
-      </div>
+    @endforeach
     </div>
   </div>
 </section>
 <!--/Team-->
+@endif
 <!--Footer-->
 <footer class="footer_wrapper" id="contact">
   <div class="container">

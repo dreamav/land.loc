399e596de6e13c7eb9507e587fabea0f63512aa9
Mevericks <reloved@gmail.com>
2018-09-27 20:56:43 +0300

    вывод ошибок

--- resources/views/site/header.blade.php
+++ resources/views/site/header.blade.php
@@ -33,3 +33,12 @@
         {{ session('status') }}
     </div>
 @endif
+@if( count($errors) > 0 )
+    <div class="alert alert-danger">
+        <ul>
+            @foreach( $errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif

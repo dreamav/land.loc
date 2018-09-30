4b6fd7f075ec13d857b6edb40fbb2ec589d4003a
Mevericks <reloved@gmail.com>
2018-09-30 14:49:41 +0300

    добавил PageController и page.blade

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Page;
+
+class PageController extends Controller
+{
+    //
+    public function execute($alias){
+
+        if(!$alias){
+            abort(404);
+        }
+
+        if(view()->exists('site.page')){
+
+            $page = Page::where('alias','like',"%$alias")->first();
+
+            $data = [
+                'title' => $page->name,
+                'page' => $page
+            ];
+            return view('site.page',$data);
+        }
+    }
+}

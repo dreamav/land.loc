a950b4c8df4df193a8d06b7871c097ac8c3ee85e
Mevericks <reloved@gmail.com>
2018-09-30 18:50:05 +0300

    главная страница админки

---
+++ resources/views/admin/header.blade.php
@@ -0,0 +1,36 @@
+<!-- Container -->
+<div class="container portfolio_title"> 
+
+<!-- Title -->
+<div class="section-title">
+	<h2>{{$title}}</h2>
+</div>
+<!--/Title --> 
+
+</div>
+<!-- Container -->
+
+<div class="portfolio-top"></div>
+
+<!-- Portfolio Filters -->
+<div class="portfolio">
+<div id="filters" class="sixteen columns">
+	<ul class="clearfix">
+		<li>
+			<a class="" href="{{route('pages')}}">
+				<h5>Страницы</h5>
+			</a>
+		</li>
+		<li>
+			<a class="" href="{{route('portfolio')}}">
+				<h5>Портфолио</h5>
+			</a>
+		</li>
+		<li>
+			<a class="" href="{{route('services')}}">
+				<h5>Сервисы</h5>
+			</a>
+		</li>
+	</ul>
+</div>
+<!--/Portfolio Filters --> 

090be5ec7964d1f8a45af1ce1a77c5228c38daf2
Mevericks <reloved@gmail.com>
2018-10-03 15:02:18 +0300

    метод execute PagesController

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -13,6 +13,15 @@ class PagesController extends Controller
     public function execute(){
         if(view()->exists('admin.pages')){
             $pages = Page::all();
+
+            $data = [
+                'title' => "Страницы",
+                'pages' => $pages
+            ];
+
+            return view('admin.pages', $data);
         }
+
+        abort(404);
     }
 }

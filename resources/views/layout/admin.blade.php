a44f0c439a281e49fe6e5f4c4d350087e411f8c6
Mevericks <reloved@gmail.com>
2018-10-03 17:43:50 +0300

    подключил bootstrap-filestyle

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -19,6 +19,7 @@
 <script type="text/javascript" src="{{asset('assets/js/jquery-1.11.0.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('assets/js/ckeditor/ckeditor.js')}}"></script>
 <script type="text/javascript" src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('assets/js/bootstrap-filestyle.min.js')}}"></script>
  
 </head>
 <body>

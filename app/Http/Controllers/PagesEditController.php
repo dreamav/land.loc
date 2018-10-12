41378e672e6a05a132ed321a5c9fc4e134524779
Mevericks <reloved@gmail.com>
2018-10-12 14:13:50 +0300

    получение данных страницы через модель Page

--- app/Http/Controllers/PagesEditController.php
+++ app/Http/Controllers/PagesEditController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Page;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
 class PagesEditController extends Controller
 {
-    public function execute(){
+    public function execute($id){
+        $page = Page::find($id);
 
+        dd($page);
     }
 }

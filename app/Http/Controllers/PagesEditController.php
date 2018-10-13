c0234c5fa61977a588bbb705979d7874457ee6f8
Mevericks <reloved@gmail.com>
2018-10-13 14:14:44 +0300

    валидация данных после редактирования страниц

--- app/Http/Controllers/PagesEditController.php
+++ app/Http/Controllers/PagesEditController.php
@@ -6,10 +6,28 @@ use App\Page;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Validator;
+
 
 class PagesEditController extends Controller
 {
     public function execute(Page $page, Request $request){
+
+        if( $request->isMethod('post') ){
+            $input = $request->except('_token');
+
+            $validator = Validator::make($input,[
+                'name'=>'required|max:255',
+                'alias'=>'required|max:255|unique:pages',
+                'text'=>'required',
+            ]);
+
+            if($validator->fails()){
+                return redirect()
+                        ->route('pagesEdit',['pages'=>$input['id']])
+                        ->withErrors($validator);
+            }
+        }
 
         $old = $page->toArray();
         if (view()->exists('admin.pages_edit')){
